67cfe7b0dd9d5804b517952f3e1ba40bab210cf8
Mayu21ad <mayuri.21ad@kct.ac.in>
2024-08-18 01:05:53 +0530

    further-commits

--- backend/blogs_app/blogs_app/settings.py
+++ backend/blogs_app/blogs_app/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = "blogs_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": '',
+        "USER": '',
+        "PASSWORD": '',
+        "HOST": '',
+        "PORT": '',
     }
 }
 
